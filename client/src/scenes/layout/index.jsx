1c5a3b2c4a891cc87e543137d840086cf7702dee
asikhsingh <amritpalasingh@outlook.com>
2022-12-31 02:21:57 -0500

    App started and tested at  1:28

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React , {useState} from 'react';
+import {Box, useMediaQuery} from "@mui/material";
+import {Outlet} from "react-router-dom";
+// import {useSelector} from "react-redux";
+import Navbar from '../../components/Navbar';
+import SideBar from '../../components/SideBar';
 
-function Layout() {
-  return (
-    <div>Layout</div>
-  )
+
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (<Box display={isNonMobile ? "flex" : "block" } width="100%" height="100%">
+  <SideBar 
+  isNonMobile={isNonMobile}
+  drawerWidth="250px"
+  isSidebarOpen={isSidebarOpen}
+  setIsSidebarOpen={setIsSidebarOpen}/>
+  
+  <Box>
+    <Navbar
+            isSidebarOpen={isSidebarOpen}
+            setIsSidebarOpen={setIsSidebarOpen}
+    
+    ></Navbar>
+    <Outlet/>
+  </Box>
+  </Box>);
 }
 
-export default Layout
+export default Layout;
