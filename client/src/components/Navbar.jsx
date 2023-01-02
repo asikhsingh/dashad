77c5a79f5d2fc47f998585bd236d09d24ec4f459
asikhsingh <amritpalasingh@outlook.com>
2023-01-02 04:41:19 -0500

    Tested 2.10

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,25 +6,37 @@ import {
   Menu as MenuIcon,
   Search,
   SettingsOutlined,
-  // ArrowDropDownOutlined,
+  ArrowDropDownOutlined,
+
   // DarkMode,
 } from "@mui/icons-material";
 
 import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "../state";
-// import profileImage from "assets/profile.jpeg";
+import profile from "../assets/profile.jpeg";
+import { useState } from "react";
 import {
   AppBar,
+  Button,
   IconButton,
+  Typography,
   Toolbar,
   useTheme,
   InputBase,
+  Menu,
+  MenuItem
 } from "@mui/material";
+import { Box } from "@mui/system";
 
-const Navbar = ({isSidebarOpen,setIsSidebarOpen}) => {
+const Navbar = ({ user, isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isOpen = Boolean(anchorEl);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
     <AppBar
@@ -53,8 +65,7 @@ const Navbar = ({isSidebarOpen,setIsSidebarOpen}) => {
           </FlexBetween>
         </FlexBetween>
         <FlexBetween>
-       
-        <IconButton onClick={() => dispatch(setMode())}>
+          <IconButton onClick={() => dispatch(setMode())}>
             {theme.palette.mode === "dark" ? (
               <DarkModeOutlined sx={{ fontSize: "25px" }} />
             ) : (
@@ -62,8 +73,55 @@ const Navbar = ({isSidebarOpen,setIsSidebarOpen}) => {
             )}
           </IconButton>
           <IconButton>
-            <SettingsOutlined sx={{fontSize: "25px"}}/>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
           </IconButton>
+          <FlexBetween>
+            <Button
+              onClick={handleClick}
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textTransform: "none",
+                gap: "1rem",
+              }}
+            >
+              <Box
+                component="img"
+                alt="progile"
+                src={profile}
+                height="32px"
+                width="32px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.85rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.75rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <ArrowDropDownOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </Button>
+            <Menu
+            anchorEl={anchorEl}
+            open={isOpen}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: "bottom" , horizontal: "center" }}>
+              <MenuItem onClick={handleClose}>Log Out</MenuItem>
+            </Menu>
+          </FlexBetween>
         </FlexBetween>
       </Toolbar>
     </AppBar>
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,104 +1,114 @@
-import React from 'react';
+import React from "react";
 import {
-    Box,
-    Divider,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-    useTheme
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  useTheme,
 } from "@mui/material";
 
 import {
-
-    ChevronLeft,
-    ChevronRightOutlined,
-    HomeOutlined,
-    ShoppingCartOutlined,
-    Groups2Outlined,
-    ReceiptLongOutlined,
-    PublicOutlined,
-    PointOfSaleOutlined,
-    TodayOutlined,
-    CalendarMonthOutlined,
-    AdminPanelSettingsOutlined,
-    TrendingUpOutlined,
-    PieChartOutlined,
-    SettingsOutlined,
+  ChevronLeft,
+  ChevronRightOutlined,
+  HomeOutlined,
+  ShoppingCartOutlined,
+  Groups2Outlined,
+  ReceiptLongOutlined,
+  PublicOutlined,
+  PointOfSaleOutlined,
+  TodayOutlined,
+  CalendarMonthOutlined,
+  AdminPanelSettingsOutlined,
+  TrendingUpOutlined,
+  PieChartOutlined,
+  SettingsOutlined,
 } from "@mui/icons-material";
-import {useEffect, useState} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
-import profile from '../assets/profile.jpeg';
+import profile from "../assets/profile.jpeg";
 
-const navItems =[
-    {
-        text:"Dashboard",
-        icon:<HomeOutlined/>,
-    },
-    {
-        text:"Client Facing",
-        icon:null,
-    }, {
-        text:"Products",
-        icon:<ShoppingCartOutlined/>,
-    }, {
-        text:"Customers",
-        icon:<Groups2Outlined/>,
-    }, {
-        text:"Transations",
-        icon:<ReceiptLongOutlined/>,
-    }, {
-        text:"Geography",
-        icon:<PublicOutlined/>,
-    }, {
-        text:"Sales",
-        icon:null,
-    }, {
-        text:"Overview",
-        icon:<PointOfSaleOutlined/>,
-    }, {
-        text:"Daily",
-        icon:<TodayOutlined/>,
-    }, {
-        text:"Monthly",
-        icon:<CalendarMonthOutlined/>,
-    }, {
-        text:"Breakdown",
-        icon:<PieChartOutlined/>,
-    }, {
-        text:"Management",
-        icon:null,
-    }, {
-        text:"Admin",
-        icon:<AdminPanelSettingsOutlined/>,
-    }, {
-        text:"Performance",
-        icon:<TrendingUpOutlined/>,
-    },
+const navItems = [
+  {
+    text: "Dashboard",
+    icon: <HomeOutlined />,
+  },
+  {
+    text: "Client Facing",
+    icon: null,
+  },
+  {
+    text: "Products",
+    icon: <ShoppingCartOutlined />,
+  },
+  {
+    text: "Customers",
+    icon: <Groups2Outlined />,
+  },
+  {
+    text: "Transations",
+    icon: <ReceiptLongOutlined />,
+  },
+  {
+    text: "Geography",
+    icon: <PublicOutlined />,
+  },
+  {
+    text: "Sales",
+    icon: null,
+  },
+  {
+    text: "Overview",
+    icon: <PointOfSaleOutlined />,
+  },
+  {
+    text: "Daily",
+    icon: <TodayOutlined />,
+  },
+  {
+    text: "Monthly",
+    icon: <CalendarMonthOutlined />,
+  },
+  {
+    text: "Breakdown",
+    icon: <PieChartOutlined />,
+  },
+  {
+    text: "Management",
+    icon: null,
+  },
+  {
+    text: "Admin",
+    icon: <AdminPanelSettingsOutlined />,
+  },
+  {
+    text: "Performance",
+    icon: <TrendingUpOutlined />,
+  },
 ];
 const SideBar = ({
-    user,
-    drawerWidth,
-    isSidebarOpen,
-    setIsSidebarOpen,
-    isNonMobile
+  user,
+  drawerWidth,
+  isSidebarOpen,
+  setIsSidebarOpen,
+  isNonMobile,
 }) => {
-    const { pathname } = useLocation();
-    const [active, setActive] = useState("");
-    const navigate = useNavigate();
-    const theme = useTheme()
+  const { pathname } = useLocation();
+  const [active, setActive] = useState("");
+  const navigate = useNavigate();
+  const theme = useTheme();
 
-    useEffect(() => {
-        setActive(pathname.substring(1));
-    }, [pathname]);
+  useEffect(() => {
+    setActive(pathname.substring(1));
+  }, [pathname]);
 
-
-return (
+  return (
     <Box component="nav">
       {isSidebarOpen && (
         <Drawer
@@ -184,29 +194,34 @@ return (
           </Box>
 
           <Box position="absolute" bottom="2rem">
-            <Divider/>
+            <Divider />
             <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
               <Box
-              component="img"
-              alt="profile"
-              src={profile}
-              height="40px"
-              borderRadius="50%"
-  
-              sx={{objectFit: "cover"}}
+                component="img"
+                alt="profile"
+                src={profile}
+                height="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
               />
-                <Box textAlign="left">
-                  <Typography fontWeight="bold" fontSize="0.9rem" sx={{ color: theme.palette.secondary[100]}}>
-                    {user.name}
-                  </Typography>
-                  <Typography fontSize="0.8rem"
-                  sx={{color: theme.palette.secondary[200]}}>
-                    {user.occupation}
-                  </Typography>
-                </Box>
-                <SettingsOutlined
-                sx={{color: theme.palette.secondary[300], fontsize:"25px"}}></SettingsOutlined>
-              
+              <Box textAlign="left">
+                <Typography
+                  fontWeight="bold"
+                  fontSize="0.9rem"
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize="0.8rem"
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{ color: theme.palette.secondary[300], fontsize: "25px" }}
+              ></SettingsOutlined>
             </FlexBetween>
           </Box>
         </Drawer>
