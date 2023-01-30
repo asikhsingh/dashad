6960868ce9b06f27a9cd245f10d4605672576b3a
asikhsingh <amritpalasingh@outlook.com>
2023-01-30 01:42:26 -0500

    Build 5.23

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,8 @@ import Transactions from "./scenes/transactions"
 import Geography from "./scenes/geography";
 import Overview from "./scenes/overview";
 import Daily from "./scenes/daily";
-// import Monthly from "scenes/monthly";
+
+import Monthly from "./scenes/monthly";
 // import Breakdown from "scenes/breakdown";
 // import Admin from "scenes/admin";
 // import Performance from "scenes/performance";
@@ -39,6 +40,7 @@ function App() {
               < Route path="/geography" element={<Geography/>} />
               < Route path="/overview" element={<Overview />} />
               < Route path="/daily" element={<Daily />} />
+              < Route path="/monthly" element={<Monthly />}/>
             
             </Route>
           </Routes>
