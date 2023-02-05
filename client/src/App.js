99fce69e2ed10e6bbd946762a4f2795db7aeb4de
asikhsingh <amritpalasingh@outlook.com>
2023-02-05 01:35:56 -0500

    render start

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ import Daily from "./scenes/daily";
 import Monthly from "./scenes/monthly";
 import Breakdown from "./scenes/breakdown";
 import Admin from "./scenes/admin";
-// import Performance from "scenes/performance";
+import Performance from "./scenes/performance";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -43,6 +43,8 @@ function App() {
               < Route path="/monthly" element={<Monthly />}/>
               < Route path="/breakdown" element={< Breakdown/>}/>
               < Route path="/admin" element={<Admin/>}/>
+              < Route path="/performance" element={ < Performance /> } />
+
             
             </Route>
           </Routes>
